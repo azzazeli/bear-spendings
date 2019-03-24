91e5c5e3bfee280adfc1c441bc43a544ee1edb57
c-alexmala <gpl3App5>
2019-03-24 17:21:09 +0200

    project setup; build is with success. nothing else work;

---
+++ spring-app/src/main/java/com/alexm/bearspendings/BearSpendingsApplication.java
@@ -0,0 +1,13 @@
+package com.alexm.bearspendings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearSpendingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BearSpendingsApplication.class, args);
+	}
+
+}
