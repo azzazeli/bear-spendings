e7e816ba6618dcbc660594e7f4ef70bf54286628
c-alexmala <gpl3App5>
2019-03-27 07:21:30 +0200

    - finished service test

--- spring-app/src/main/java/com/alexm/bearspendings/entity/Product.java
+++ spring-app/src/main/java/com/alexm/bearspendings/entity/Product.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 /**
  * @author AlexM
@@ -20,6 +21,11 @@ public class Product {
     private String comment;
     //todo: list of optional product properties
     //todo product builder
+
+
+    public Product(String name) {
+        this.name = name;
+    }
 
     public Product() {
     }
@@ -55,5 +61,25 @@ public class Product {
                 ", comment='" + comment+ '\'' +
                 ", name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (!Objects.equals(id, product.id)) return false;
+        if (!Objects.equals(name, product.name)) return false;
+        return Objects.equals(comment, product.comment);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (comment != null ? comment.hashCode() : 0);
+        return result;
     }
 }
--- spring-app/src/test/java/com/alexm/bearspendings/service/ProductServiceImplTest.java
+++ spring-app/src/test/java/com/alexm/bearspendings/service/ProductServiceImplTest.java
@@ -3,15 +3,14 @@ package com.alexm.bearspendings.service;
 import com.alexm.bearspendings.entity.Product;
 import com.alexm.bearspendings.repository.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
+import static org.hamcrest.MatcherAssert.assertThat;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.mockito.Mockito.when;
 
 /**
@@ -21,22 +20,18 @@ import static org.mockito.Mockito.when;
 class ProductServiceImplTest {
     @Mock
     ProductRepository productRepository;
-
-//    @InjectMocks
     ProductService productService;
-
 
     @BeforeEach
     void setUp() {
         productService = new ProductServiceImpl(productRepository);
         when(productRepository.findAll()).thenReturn(Arrays.asList(
-                new Product()
+                new Product("Lapte")
         ));
     }
 
-
     @Test
     void products() {
-//        assertEquals(productService.products()).con
+        assertThat(productService.products(), containsInAnyOrder(new Product("Lapte")));
     }
 }
