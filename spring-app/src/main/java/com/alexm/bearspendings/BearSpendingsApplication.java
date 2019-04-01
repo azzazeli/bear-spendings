b936420b00a6276137b9aad57d54a23e781fe538
c-alexmala <gpl3App5>
2019-04-01 15:42:28 +0300

    disable spring security

--- spring-app/src/main/java/com/alexm/bearspendings/BearSpendingsApplication.java
+++ spring-app/src/main/java/com/alexm/bearspendings/BearSpendingsApplication.java
@@ -2,12 +2,18 @@ package com.alexm.bearspendings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {SecurityAutoConfiguration.class}
+)
 public class BearSpendingsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BearSpendingsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BearSpendingsApplication.class, args);
+    }
 
 }
