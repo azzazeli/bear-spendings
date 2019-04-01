f799914faae81514b87f4b1495723838cd7d017d
c-alexmala <gpl3App5>
2019-04-01 15:58:13 +0300

    ui: 3 new components as prototype, simple routing

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bear-spendings
 Simple app that monitor bear spendings. 
+
+##TODO
+
+1. Add bootstrap css to Ui framework
+2. Build header of Ui app
+3. Build add bill form
--- angular-ui/src/app/app-routing.module.ts
+++ angular-ui/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AddBillComponent } from './add-bill/add-bill.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'add-bill', component: AddBillComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
