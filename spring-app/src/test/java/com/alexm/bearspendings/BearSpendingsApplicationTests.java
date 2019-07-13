c9be9b89e8e1cbdf43e5a71121605edb42cd36a4
c-alexmala <gpl3App5>
2019-07-13 11:09:54 +0300

    closes #11: init in memory h2 with data. write a simple assertion test

--- spring-app/src/test/java/com/alexm/bearspendings/BearSpendingsApplicationTests.java
+++ spring-app/src/test/java/com/alexm/bearspendings/BearSpendingsApplicationTests.java
@@ -1,14 +1,40 @@
 package com.alexm.bearspendings;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 public class BearSpendingsApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Autowired
+	WebApplicationContext webAppContext;
+
+	private MockMvc mvc;
+
+	@BeforeEach
+	public void setupMvc() {
+		this.mvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+	}
+
+	@Test
+	public void getAllBills() throws Exception {
+		this.mvc.perform(get("/bills"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.length()").value("2"))
+		;
 	}
 
 }
---
+++ spring-app/src/test/resources/data.sql
@@ -0,0 +1,14 @@
+
+insert into store (id, city, country, location, name) values (1, 'chisinau', 'moldova', 'botanica viaduct', 'nr.1');
+
+insert into product(id, name) values (1, 'chefir jlc 1%');
+insert into product(id, name) values (2, 'chefir jlc 2.5%');
+insert into product(id, name) values (3, 'Inghetata Beriozka 1kg');
+
+insert into bill(id, order_date, store_id) values (1, '2019-04-18', 1);
+insert into bill(id, order_date, store_id) values (2, '2019-04-22', 1);
+
+insert into bill_item(id, price, quantity, product_id, bill_id) values (1, 7.85, 1, 1, 1);
+insert into bill_item(id, price, quantity, product_id, bill_id) values (2, 9.85, 1, 2, 1);
+
+insert into bill_item(id, price, quantity, product_id, bill_id) values (3, 87.00, 1, 3, 2);
