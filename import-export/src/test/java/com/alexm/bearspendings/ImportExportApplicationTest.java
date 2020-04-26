283c3408524d6a046ccad0b97349b637c3bb6026
c-alexmala <gpl3App5>
2020-04-26 14:33:22 +0300

    #68 integration test

--- import-export/src/test/java/com/alexm/bearspendings/ImportExportApplicationTest.java
+++ import-export/src/test/java/com/alexm/bearspendings/ImportExportApplicationTest.java
@@ -1,9 +1,18 @@
 package com.alexm.bearspendings;
 
+import com.alexm.bearspendings.imports.BillImporter;
+import com.alexm.bearspendings.imports.ImportsException;
+import com.alexm.bearspendings.service.BillService;
+import com.alexm.bearspendings.service.ProductService;
+import com.alexm.bearspendings.service.StoreService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.ResourceUtils;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author AlexM
@@ -12,8 +21,23 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class ImportExportApplicationTest {
 
-    @Test
-    void contextLoads() { //NOSONAR
-    }
+    @Autowired
+    BillImporter importer;
 
+    @Autowired
+    BillService billService;
+
+    @Autowired
+    ProductService productService;
+
+    @Autowired
+    StoreService storeService;
+
+    @Test
+    void importsBills() throws FileNotFoundException, ImportsException { //NOSONAR
+        importer.imports(ResourceUtils.getFile("classpath:import_sample.xlsm").toPath());
+        assertEquals(3L, billService.allBillsCount().longValue());
+        assertEquals(21L, productService.products().size());
+        assertEquals(2L, storeService.allStores().size());
+    }
 }
