6a64da848e382ccd1a8895dbd636085ff5c55543
c-alexmala <gpl3App5>
2020-12-08 10:59:09 +0200

    download file from server in ui - done

--- angular-ui/src/app/header/header.component.ts
+++ angular-ui/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NGXLogger} from 'ngx-logger';
 import {BillService} from '../core/service/bill.service';
 
@@ -9,14 +9,29 @@ import {BillService} from '../core/service/bill.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private logger: NGXLogger, private billService: BillService) { }
+  constructor(private logger: NGXLogger, private billService: BillService) {
+  }
 
   ngOnInit() {
   }
 
   exportAll() {
-    this.logger.debug('Exporting all bills ...');
-    this.billService.exportAll();
-  }
+    this.billService.exportAll().subscribe(response => {
+        this.logger.log('success', response);
+        const blob: Blob = new Blob([response], { type: 'application/vnd.ms-excel.sheet.macroEnabled.12' });
+        const fileName = 'export-all.xlsm';
+        const objectUrl: string = URL.createObjectURL(blob);
+        const a: HTMLAnchorElement = document.createElement('a') as HTMLAnchorElement;
 
+        a.href = objectUrl;
+        a.download = fileName;
+        document.body.appendChild(a);
+        a.click();
+
+        document.body.removeChild(a);
+        URL.revokeObjectURL(objectUrl);
+
+      }, (error) => this.logger.log(`Error downloading the file ${error}`),
+      () => this.logger.info('File downloaded successfully'));
+  }
 }
