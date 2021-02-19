33e8e9e0978e61a6a1d81521ac968ab9102981ca
c-alexmala <gpl3App5>
2021-02-19 17:42:10 +0200

    issue #35 already closed - removed unnecessary todo

--- core/src/main/java/com/alexm/bearspendings/entity/BillItem.java
+++ core/src/main/java/com/alexm/bearspendings/entity/BillItem.java
@@ -28,10 +28,6 @@ public class BillItem extends BaseEntity {
     @Builder.Default
     private Double totalPrice = 0.0;
 
-    //TODO: please implement m2: issue #35
-    // spring jpa generate n+1 query; @FetchMode does not work properly
-    // try to use @NamedEntityGraph and @EntityGraph
-    // details here: https://stackoverflow.com/questions/29602386/how-does-the-fetchmode-work-in-spring-data-jpa
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "bill_id")
     private Bill bill;
